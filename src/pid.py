874a4acafd3543c5ee5ff2db61e38c9116c55ffe
christophw06 <christoph.weiss.bw@gmail.com>
2026-06-03 15:19:41 +0200

    Added queue to avareg calculation -> now possible to calc average

--- src/pid.py
+++ src/pid.py
@@ -30,19 +30,13 @@ def get_value_sensor_in_list(
             sensor_values.put(sensor_value_converted)
             time.sleep((readin_timestep_in_ms / number_of_values_to_readin) / 100)
 
-        print(list(sensor_values.queue))
 
-
-"!!!average calculating extend and correct with queue -> threading with both functions and one list possible"
-
-
-def average_value(values_list):
+def calc_average_value(values_list):
     while True:
-        number_to_divide = len(values_list)
+        number_to_divide = values_list.qsize()
         if number_to_divide > 0:
-            average_value = sum(values_list) / number_to_divide
+            average_value = sum(list(values_list.queue)) / number_to_divide
             print(average_value)
-            return average_value
 
 
 values_to_process = threading.Thread(
@@ -50,7 +44,8 @@ values_to_process = threading.Thread(
     args=(sensor.sensor_line, "mid", 200, 10),
 )
 
-# calculate_average = threading.Thread(target=average_value, args=sensor_values)
+calculate_average = threading.Thread(target=calc_average_value, args=(sensor_values,))
+
 
 values_to_process.start()
-# calculate_average.start()
+calculate_average.start()
